1b7dd3a4783de431a6c9bf76a3279f9387b747f0
Yuji Shimoda <yuji.shimoda@gmail.com>
2014-10-04 14:02:12 +0900

    add bootstrap files & top controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # ログイン／ユーザ登録済みチェック
+  def signed_in?
+    User.where(id: session[:user_id]).exists?
+  end
+  helper_method :signed_in?
+
 end
---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,5 @@
+class TopController < ApplicationController
+  #skip_before_filter :authenticate
+  def index
+  end
+end
