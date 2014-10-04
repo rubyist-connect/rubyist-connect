64c8b0b496fb55f0ddc44f75e6e8998b0d1528e0
Yuji Shimoda <yuji.shimoda@gmail.com>
2014-10-04 14:44:57 +0900

    User model add colum github_id.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  def find_or_create_from_auth_hash
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'top#index'
   get '/auth/:provider/callback', to: 'sessions#create'
 
   # Example of regular route:
