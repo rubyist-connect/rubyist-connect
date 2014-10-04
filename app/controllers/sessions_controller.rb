da680dc549a2fd5bdd9a7bb776e4acc5b6964bef
Yuji Shimoda <yuji.shimoda@gmail.com>
2014-10-04 13:34:18 +0900

    add omniauth & sessions controller.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    self.current_user = @user
+    redirect_to '/nnect'
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+  
+end
