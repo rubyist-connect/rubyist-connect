876462ce129736f3c7c41a441e654d6ac8278ce7
Yuji Shimoda <yuji.shimoda@gmail.com>
2014-10-04 15:58:22 +0900

    users/index.html.erb update.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'top#index'
   get '/auth/:provider/callback', to: 'sessions#create'
-  get '/nnect' => 'user#index'
+  get '/nnect' => 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
