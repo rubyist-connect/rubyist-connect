0a2371ae7761fa9df1b127bbf3602a5db8080206
Yuji Shimoda <yuji.shimoda@gmail.com>
2014-10-05 08:13:51 +0900

    user edit & show implementation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,15 @@ class UsersController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
-  
+
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_user
@@ -21,6 +28,7 @@ class UsersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params[:user]
+    #params[:user]
+    params.require(:user).permit(:introduction)
   end
 end
