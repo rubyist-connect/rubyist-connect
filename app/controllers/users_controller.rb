ea6a2fac6629150a144f8a881d82c141d1c79700
hilohiro <hilohiro+bitbucket@gmail.com>
2014-10-05 09:24:07 +0900

    Fixed unauthorized access to pages only for logged in users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authorize_access
+
   def index
     @users = User.all
+  end
+
+  private
+
+  def authorize_access
+    redirect_to root_path unless signed_in?
   end
 end
