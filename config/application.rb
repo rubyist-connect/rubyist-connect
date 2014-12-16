6d6ff132113357fe8ff519954a7ed324fdd45040
Junichi Ito <jit@sonicgarden.jp>
2014-12-17 07:58:54 +0900

    Remove empty files./Arrange application settings.

--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,23 @@ Bundler.require(*Rails.groups)
 
 module RubyistConnect
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Asia/Tokyo'
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.i18n.default_locale = :ja
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       controller_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false
+      g.assets false
+      g.helper false
+      g.javascripts false
+      g.stylesheets false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
