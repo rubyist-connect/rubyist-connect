cff076f51a0b73bba430871b64a07700b115d9d0
Junichi Ito <jit@sonicgarden.jp>
2014-12-21 05:54:40 +0900

    Remove comments, etc.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,33 +4,20 @@ class User < ActiveRecord::Base
   has_many :interests
 
   def self.find_or_create_from_auth_hash(auth_hash)
-    find_by_github_id(auth_hash["uid"]) || create_with_omniauth(auth_hash)
+    find_by_github_id(auth_hash['uid']) || create_with_omniauth(auth_hash)
   end
 
   def self.create_with_omniauth(oauth)
-    # auth情報登録
     user = User.new
-    user.github_id      = oauth["uid"]
+    user.github_id = oauth['uid']
 
-    if oauth["info"].present?
-      user.name       = oauth["info"]["name"]
-      user.image      = oauth["info"]["image"]
-      user.email      = oauth["info"]["email"]
-      user.nickname   = oauth["info"]["nickname"]
-      user.location   = oauth["info"]["location"]
-      user.github_url = oauth["info"]["urls"]["GitHub"]
-      user.blog       = oauth["info"]["urls"]["Blog"]
-    end
-
-    # if oauth["credentials"].present?
-    #   user.token  = oauth['credentials']['token']
-    #   user.secret = oauth['credentials']['secret']
-    # end
-    #
-    # if oauth["extra"].present? and oauth["extra"]["raw_info"].present?
-    #   user.gender   = oauth["extra"]["raw_info"]["gender"]
-    #   user.location = oauth["extra"]["raw_info"]["location"] if user.location.blank?
-    # end
+    user.name       = oauth['info']['name']
+    user.image      = oauth['info']['image']
+    user.email      = oauth['info']['email']
+    user.nickname   = oauth['info']['nickname']
+    user.location   = oauth['info']['location']
+    user.github_url = oauth['info']['urls']['GitHub']
+    user.blog       = oauth['info']['urls']['Blog']
 
     user.save!
 
