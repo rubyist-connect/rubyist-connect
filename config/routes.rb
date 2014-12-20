d8b5a1cd88c3a12b075ea11b1f009f6193566be2
Junichi Ito <jit@sonicgarden.jp>
2014-12-21 04:44:28 +0900

    Organize root.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     delete '/users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
+
   patch '/nnect/:id' => 'users#update'
-  put '/nnect/:id' => 'users#update'
   resources :users , path: "nnect" , only: [:index, :show, :destroy], param: :nickname
   resources :interests, path: "tags" , only: [:new, :create, :index, :update, :destroy]
   get '/tag_settings' => 'interests#edit'
