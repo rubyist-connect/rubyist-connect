65cc3fded3e5a6f97e7a14d1e6fa5439b61d19df
Junichi Ito <jit@sonicgarden.jp>
2014-12-21 05:06:26 +0900

    Remove after sign in path for.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
-
-  def after_sign_in_path_for(resource)
-    users_path
-  end
 end
