a936ff569fae02510645b856cd8bbe7e237b4438
izumin5210 <masayuki@izumin.info>
2015-01-10 14:57:03 +0900

    Add events#new and events#create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,27 @@ class EventsController < ApplicationController
     @events = Event.includes(participations: [:user])
       .all.order(created_at: :asc)
   end
+
+  def new
+    @event = Event.new
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event, notice: 'Event was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, user_ids: [])
+  end
 end
