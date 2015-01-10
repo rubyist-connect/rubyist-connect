8d9866e59d5e694f41ebf4272f1f93731b504ce6
やましー <3.1415mailadress@gmail.com>
2015-01-10 15:48:15 +0900

    navbarのデザインを変更

---
+++ fixBrewLionPostgresql.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+BREW_POSTGRES_DIR=`brew info postgres | awk '{print $1"/bin"}' | grep "/postgresql/"`
+LION_POSTGRES_DIR=`which postgres | xargs dirname`
+LION_PSQL_DIR=`which psql | xargs dirname`
+
+sudo mkdir -p $LION_POSTGRES_DIR/archive
+sudo mkdir -p $LION_PSQL_DIR/archive
+
+for i in `ls $BREW_POSTGRES_DIR`
+do
+	if [ -f $LION_POSTGRES_DIR/$i ] 
+	then
+		sudo mv $LION_POSTGRES_DIR/$i $LION_POSTGRES_DIR/archive/$i
+		sudo ln -s $BREW_POSTGRES_DIR/$i $LION_POSTGRES_DIR/$i
+	fi
+	
+	if [ -f $LION_PSQL_DIR/$i ] 
+	then
+		sudo mv $LION_PSQL_DIR/$i $LION_PSQL_DIR/archive/$i
+		sudo ln -s $BREW_POSTGRES_DIR/$i $LION_PSQL_DIR/$i
+	fi	
+done
