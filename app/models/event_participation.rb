5de79bbd1cf2ae21bd3dfaf8e3f974f1feff6ed3
izumin5210 <masayuki@izumin.info>
2015-01-10 17:10:40 +0900

    Add TODO comment to Event model

--- app/models/event_participation.rb
+++ app/models/event_participation.rb
@@ -1,4 +1,8 @@
 class EventParticipation < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
+
+  # TODO: Event.create(name: 'hoge', user_ids: [1, 2]) がエラーになる
+  # validates :event_id, presence: true
+  # validates :user_id, presence: true
 end
