1aa9fb5537ef897c2234aaa62f42072ec86bc3f3
Junichi Ito <jit@sonicgarden.jp>
2015-01-13 06:34:12 +0900

    Add spec.

--- spec/features/top_spec.rb
+++ spec/features/top_spec.rb
@@ -8,4 +8,15 @@ feature 'Top spec' do
 
     expect(current_path).to eq users_path
   end
+
+  scenario 'activeなユーザーが表示されること' do
+    create :inactive_user
+    active = create :user
+    visit root_path
+    within '.new-members' do
+      imgs = all('img')
+      expect(imgs.size).to eq 1
+      expect(imgs.first['alt']).to eq active.name
+    end
+  end
 end
