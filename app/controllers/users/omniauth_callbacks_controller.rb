388436cff614f8b4090d0f189717af3833006396
Yohsuke Murase <yohsuke.murase@gmail.com>
2015-01-12 09:50:38 +0900

    i18n

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     unless user.active?
       session[:user_return_to] = edit_user_path
-      flash[:alert] = '自己紹介を入力してください'
+      flash[:alert] = t('flash_messages.warning_for_nonactive_users')
     end
 
     sign_in_and_redirect user, event: :authentication #this will throw if @user is not activated
