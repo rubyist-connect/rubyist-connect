d98806020325c39b2e74c5941502b8a97746831f
Junichi Ito <jit@sonicgarden.jp>
2015-01-13 06:25:20 +0900

    Fix spec.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,19 +1,19 @@
 FactoryGirl.define do
   factory :user do
-    github_id Faker::Address.zip_code # idの代用
-    nickname Faker::Internet.slug
+    github_id { Faker::Address.zip_code } # idの代用
+    nickname { Faker::Internet.slug(nil, '_') }
 
-    name Faker::Name.name
-    image Faker::Avatar.image
-    email Faker::Internet.email
-    introduction Faker::Lorem.paragraph
-    twitter_name Faker::Internet.slug
-    github_url "https://github.com/#{Faker::Internet.slug}"
-    facebook_name Faker::Internet.slug
-    location Faker::Address.city
-    blog Faker::Internet.url
-    birthday Faker::Date.between(60.years.ago, 10.years.ago)
-    qiita_name Faker::Internet.slug
+    name { Faker::Name.name }
+    image { Faker::Avatar.image }
+    email { Faker::Internet.email }
+    introduction { Faker::Lorem.paragraph }
+    twitter_name { Faker::Internet.slug }
+    github_url { "https://github.com/#{Faker::Internet.slug}" }
+    facebook_name { Faker::Internet.slug }
+    location { Faker::Address.city }
+    blog { Faker::Internet.url }
+    birthday { Faker::Date.between(60.years.ago, 10.years.ago) }
+    qiita_name { Faker::Internet.slug }
 
     factory :inactive_user do
       name nil
