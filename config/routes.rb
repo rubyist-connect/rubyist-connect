471895ed263a45a97dca9b9fd86592a2198576df
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-02-08 17:06:46 +0900

    issue #134

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   devise_scope :user do
-    delete '/users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+    delete '/users/sign_out', to: 'sessions#destroy', as: :destroy_user_session
   end
 
   scope 'nnect' do
