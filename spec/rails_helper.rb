500b93aa8a4dfaa55339a71a2c1b4a3ae9f16ffa
Junichi Ito <jit@sonicgarden.jp>
2015-05-18 15:26:24 +0900

    Change transaction settings.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,7 +17,7 @@ RSpec.configure do |config|
 
   config.include LoginMacros, type: :feature
 
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   config.infer_spec_type_from_file_location!
 
