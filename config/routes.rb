49b47ff1270c0e63e745598471a179ed35c7a3fd
Yuji Shimoda <yuji.shimoda@gmail.com>
2016-06-04 23:20:34 +0900

    Support Let's Encrypt
    Add PagesController for acme-challenge request

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     delete '/users/sign_out', to: 'sessions#destroy', as: :destroy_user_session
   end
 
+  get '/.well-known/acme-challenge/:id' => 'pages#letsencrypt'
   scope 'nnect' do
     resources :events do
       resources :users, only: %i(show), param: :nickname
