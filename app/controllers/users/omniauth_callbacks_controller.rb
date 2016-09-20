7dedfddc3cf52e46cca3e7a7d1d65117e5e48d7f
Yuji Shimoda <yuji.shimoda@gmail.com>
2016-09-21 01:43:09 +0900

    redirect to users#show from top page

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:alert] = t('flash_messages.warning_for_nonactive_users')
     end
 
-    sign_in_and_redirect user, event: :authentication #this will throw if @user is not activated
+    nickname = request.env['omniauth.params']['nickname']
+    if nickname == nil
+      sign_in_and_redirect user, event: :authentication #this will throw if @user is not activated
+    else
+      sign_in user
+      redirect_to user_path(nickname), event: :authentication
+    end
 
     # TODO 認証を拒否した場合のフローも必要な気がするが、GitHubは拒否するボタンがないのでテストできない？
     # 以下は公式ページのサンプルコード
