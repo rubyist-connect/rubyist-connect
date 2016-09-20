84e8f7577ab0a621c9bde70f565f6fb1326c06bd
Yuji Shimoda <yuji.shimoda@gmail.com>
2016-09-20 20:54:52 +0900

    move current_user_profile method from users_controller to application_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,16 +40,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def current_user_profile
-    @current_user_profile = set_user
-  end
-
-  private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = current_user
-  end
-
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
     params.require(:user).permit(:introduction, :twitter_name, :facebook_name, :qiita_name, :location, :name, :blog, :birthday, :new_user_notification_enabled, :email)
