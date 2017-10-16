9f2183ce87982fba72dc17b57b48821abef17acc
Junichi Ito <jit@sonicgarden.jp>
2017-10-17 07:27:06 +0900

    Run specs in random

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,6 +38,8 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.order = :random
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
