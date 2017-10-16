cb43d95cd8b1dfcadc4acb5a80fcc66068f6f0b4
Junichi Ito <jit@sonicgarden.jp>
2017-10-17 07:39:09 +0900

    Set new_session_path for Omniauth error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,9 @@ class ApplicationController < ActionController::Base
   def prepare_search_users
     @q = User.order(:id).reverse_order.without_login_user(current_user).ransack(params[:q])
   end
+
+  # https://github.com/plataformatec/devise/wiki/OmniAuth%3A-Overview#using-omniauth-without-other-authentications
+  def new_session_path(scope)
+    root_path
+  end
 end
