80496cf89d0b8ffee8be5b4e5c6e0181341211b6
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2019-10-11 11:30:04 +0900

    Add manifest.js for Sprockets4

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,14 @@
+// app/assets/config/manifest.js
+//
+//
+//= link application.css
+//= link starter-template.css
+//
+//= link application.js
+//
+//= link favicon.ico
+//= link touch-icon-iphone.png
+//= link touch-icon-ipad.png
+//= link touch-icon-iphone-retina.png
+//= link touch-icon-ipad-retina.png
+//= link loading.gif
