ad67ce169ad8cc1fd04c581ed5cdcfe641f2267a
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2019-10-18 11:05:38 +0900

    Remove unnecessary host settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts = "rubyist.co"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,8 +5,6 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "www.example.com"
-  config.hosts << "127.0.0.1"
 
   config.cache_classes = true
 
