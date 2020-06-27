5c40f9b7ccf0f9161c51ed86f219ebf67e9959f7
soruma <soruma.net@gmail.com>
2020-06-27 10:24:08 +0900

    Comment out log_tags for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,8 @@ Rails.application.configure do
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  # config/initializers/logging.rb で設定している
+  # config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
