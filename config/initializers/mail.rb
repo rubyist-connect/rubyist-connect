585a39cde6bc14b195e25b7f6959470e92584dfc
Junichi Ito <jit@sonicgarden.jp>
2021-02-11 19:01:44 +0900

    Migrate to Mailgun

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,12 +1,8 @@
 if Rails.env.staging? or Rails.env.production?
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => ENV['HOST_NAME'],
-    :authentication => :plain,
+  ActionMailer::Base.delivery_method = :sendgrid_actionmailer
+  ActionMailer::Base.sendgrid_actionmailer_settings = {
+    api_key: ENV['SENDGRID_API_KEY'],
+    raise_delivery_errors: true
   }
-  ActionMailer::Base.delivery_method = :smtp
 end
 Rails.application.config.action_mailer.default_url_options = Rails.application.default_url_options = { protocol: 'https', host: Settings.host_name }
